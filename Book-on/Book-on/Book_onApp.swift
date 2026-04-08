8c8f168d7e5825edd58d233bb99af9e5a6230569
minjjun0317 <minjjun0317@gmail.com>
2026-04-09 08:22:08 +0900

    fix: remove submodule issue

---
+++ Book-on/Book-on/Book_onApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_onApp.swift
+//  Book-on
+//
+//  Created by 김민준 on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Book_onApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book-on/Book-on/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Book-on
+//
+//  Created by 김민준 on 4/8/26.
+//
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        ZStack () {
+            Color.black.ignoresSafeArea()
+            
+            VStack (spacing:0) {
+                HStack () {
+                    Image ("book-on" )
+                        .padding(.trailing, 9)
+                    Text ("Book-on" )
+                        .foregroundColor (Color.white)
+                        .font(.system(size:17, weight: .semibold))
+                    Spacer ()
+                }
+                .padding (.leading, 25)
+                Spacer ()
+            }
+            VStack () {
+                Text("학교 도서관을 더 쉽게 사용하는 방법" )
+                    .foregroundColor(.white)
+                    .padding (.bottom, 41)
+                    .font(.system(size:17,weight: .bold))
+                Text("책 검색, 대출현황, 좌석예약까지\n한 번에 관리하세요")
+                    .foregroundColor(.white)
+                    .padding(.bottom, 64)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size:17, weight: .bold))
+                Text("시작하기")
+                    .foregroundColor(.white)
+                    .frame (width: 162, height: 32)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke (Color.white, lineWidth: 2)
+                        
+                        )
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
