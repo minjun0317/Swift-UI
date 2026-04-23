860501cfc62b8550a3f13f9c92ee1a9072c9f0f2
minjjun0317 <minjjun0317@gmail.com>
2026-04-23 12:27:49 +0900

    정리

--- Ganadi/Ganadi/ContentView.swift
+++ Ganadi/Ganadi/ContentView.swift
@@ -6,35 +6,79 @@
 //
 
 import SwiftUI
-struct ContentView2: View {
+
+struct firstView: View {
     var body: some View {
-        ZStack() {
-            Color.black.ignoresSafeArea()
-            
-            VStack(spacing:0) {
-                Image("ganadi")
-                    .foregroundStyle(.tint)
-                    .padding(.bottom, 170)
+        NavigationStack {
+            ZStack() {
+                Color.black.ignoresSafeArea()
+                
+                VStack(spacing:0) {
+                    Image("hamstar")
+                        .foregroundStyle(.tint)
+                        .padding(.bottom, 170)
+                    VStack {
+                        NavigationLink {
+                            ContentView()
+                        } label: {
+                                Text("시작하기")
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 30, weight: .bold))
+                                    .frame(width: 200, height: 50)
+                                    .background(.darkgray)
+                                    .cornerRadius(30)
+                                    .shadow(color: .white.opacity(0.5), radius: 10, x: 0, y: 5)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 30)
+                                            .stroke(.darkgray, lineWidth: 1)
+                                )
+                        }
+                        .padding(.top, 140)
+                    }
+                }
             }
-            
-            VStack {
-                Text("Crying")
-                    .foregroundStyle(.white)
-                    .font(.system(size: 30, weight: .thin))
-                    .frame(width: 200, height: 50)
-                    .background(.darkgray)
-                    .cornerRadius(30)
-                    .shadow(color: .white.opacity(0.5), radius: 10, x: 0, y: 5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 30)
-                            .stroke(.darkgray, lineWidth: 1)
-                    )
-            }
-            .padding(.top, 240)
         }
     }
 }
 
 #Preview {
-    ContentView2()
+    firstView()
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack() {
+                Color.black.ignoresSafeArea()
+                
+                VStack(spacing:0) {
+                    Image("ganadi")
+                        .foregroundStyle(.tint)
+                        .padding(.bottom, 170)
+                }
+                
+                VStack {
+                    NavigationLink {
+                    } label: {
+                            Text("Crying")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 30, weight: .thin))
+                                .frame(width: 200, height: 50)
+                                .background(.darkgray)
+                                .cornerRadius(30)
+                                .shadow(color: .white.opacity(0.5), radius: 10, x: 0, y: 5)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .stroke(.darkgray, lineWidth: 1)
+                            )
+                    }
+                    .padding(.top, 240)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
 }
